f1e5be5c11e4b908eb7dd91174c2fbd5f86fb9ce
badbadgao <wave.gao2013@gmail.com>
2019-07-10 22:20:21 +1200

    Fix header error

--- src/DetailsScreen/index.js
+++ src/DetailsScreen/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { View, Text, Button } from 'react-native';
 import { Container, Content } from 'native-base';
-import DetailsHeader from './DetailsHeader';
 
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    paddingTop: -10
+    flex: 1,
   }
 }); 
 
